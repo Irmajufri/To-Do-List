3ec2ded1d25aca95d94e580f382e24a3ba82f751
Irmajufri <jufrii17d@student.unhas.ac.id>
2020-05-29 20:27:30 +0800

    initial commit

---
+++ app/src/main/java/com/irmajufri/todolist/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.irmajufri.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
